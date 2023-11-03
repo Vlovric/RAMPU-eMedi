3b3abf6e308314718ca09ebd70d4cc242674a69b
David Matijanić <dmatijani21@foi.hr>
2023-11-03 19:20:50 +0100

    Napravljena navigabilnost između 5 fragmenata: DoctorsFragment, AppointmentsFragment, HomeFragment, ProfileFragment i SettingsFragment.

--- Software/app/src/main/java/hr/foi/rampu/emedi/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/MainActivity.kt
@@ -2,10 +2,36 @@ package hr.foi.rampu.emedi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import hr.foi.rampu.emedi.adapters.MainPagerAdapter
+import hr.foi.rampu.emedi.fragments.AppointmentsFragment
+import hr.foi.rampu.emedi.fragments.DoctorsFragment
+import hr.foi.rampu.emedi.fragments.HomeFragment
+import hr.foi.rampu.emedi.fragments.ProfileFragment
+import hr.foi.rampu.emedi.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+        mainPagerAdapter.addFragment(MainPagerAdapter.FragmentItem(R.string.doctors, DoctorsFragment::class))
+        mainPagerAdapter.addFragment(MainPagerAdapter.FragmentItem(R.string.appointments, AppointmentsFragment::class))
+        mainPagerAdapter.addFragment(MainPagerAdapter.FragmentItem(R.string.home, HomeFragment::class))
+        mainPagerAdapter.addFragment(MainPagerAdapter.FragmentItem(R.string.profile, ProfileFragment::class))
+        mainPagerAdapter.addFragment(MainPagerAdapter.FragmentItem(R.string.settings, SettingsFragment::class))
+
+        tabLayout = findViewById(R.id.tl_navigation)
+        viewPager2 = findViewById(R.id.vp_fragments)
+
+        viewPager2.adapter = mainPagerAdapter
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
+        }.attach()
     }
 }
