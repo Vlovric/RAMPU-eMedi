5195ae07fecea94fad684f85d3a2590e4be9af07
Domagoj Hegedušić <dhegedusi21@foi.hr>
2023-11-10 15:38:37 +0100

    Optimized code.

--- Software/app/src/main/java/hr/foi/rampu/emedi/AddReviewActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/AddReviewActivity.kt
@@ -20,43 +20,37 @@ class AddReviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_review)
 
-        // Initialize views from add_review.xml
         ratingBar = findViewById(R.id.ratingBar)
         editTextReview = findViewById(R.id.editTextReview)
         submitButton = findViewById(R.id.submitButton)
 
-        // Set click listener for the submit button
+
         submitButton.setOnClickListener {
             addReviewAndDisplayAllReviews()
         }
     }
 
     private fun addReviewAndDisplayAllReviews() {
-        // Get user input
+
         val userGrade = ratingBar.rating.toInt()
         val userDescription = editTextReview.text.toString()
 
-        // Create a new review
+
         val userReview = Review(userGrade, userDescription)
 
-        // Add the new review
         Review.addReview(userReview)
 
-        // Display all reviews in the ListView from all_reviews.xml
         displayAllReviews()
     }
 
     private fun displayAllReviews() {
-        // Set the content view to all_reviews.xml
+
         setContentView(R.layout.all_reviews)
 
-        // Retrieve all reviews from the Review companion object
         val allReviews = Review.getAllReviews()
 
-        // Find the ListView by its ID in the current layout
         listViewReviews = findViewById(R.id.listViewReviews)
 
-        // Create and set the adapter for the ListView
         val adapter = ReviewAdapter(this, allReviews)
         listViewReviews.adapter = adapter
     }
