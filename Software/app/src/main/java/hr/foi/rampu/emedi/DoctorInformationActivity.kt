466a75bb3eb9dd0a1d033fdd10b03f1e671a9dc7
Viktor Lovric <viktorlovric21@gmail.com>
2023-11-15 22:20:40 +0100

    Prenosenje objekta doktora intentom i upisivanje podataka na ekran podataka doktora

--- Software/app/src/main/java/hr/foi/rampu/emedi/DoctorInformationActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/DoctorInformationActivity.kt
@@ -1,5 +1,6 @@
 package hr.foi.rampu.emedi
 
+import Doctor
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,31 @@ class DoctorInformationActivity : AppCompatActivity() {
         btnCheckReviews.setOnClickListener {
             checkReviews()
         }
+
+        val receivedDoctor = intent.getParcelableExtra<Doctor>("doctor")
+
+
+        // inicijaliziram textview-ove
+        val tvName = findViewById<TextView>(R.id.tv_dynamic_name)
+        val tvSurname = findViewById<TextView>(R.id.tv_dynamic_surname)
+        val tvSpecialization = findViewById<TextView>(R.id.tv_dynamic_specialization)
+        val tvYearsEmployed = findViewById<TextView>(R.id.tv_dynamic_years)
+        val tvJobDescription = findViewById<TextView>(R.id.tv_dynamic_job_description)
+        val tvClinicName = findViewById<TextView>(R.id.tv_dynamic_clinic_name)
+        val tvAddress = findViewById<TextView>(R.id.tv_dynamic_address)
+        val tvEmail = findViewById<TextView>(R.id.tv_dynamic_email)
+        val tvTelephone = findViewById<TextView>(R.id.tv_dynamic_telephone)
+
+        // učitavam u text-view
+        tvName.text = receivedDoctor?.name.orEmpty()
+        tvSurname.text = receivedDoctor?.surname.orEmpty()
+        tvSpecialization.text = receivedDoctor?.specialization.orEmpty()
+        tvYearsEmployed.text = "${receivedDoctor?.yearsEmployed ?: 0}"
+        tvJobDescription.text = receivedDoctor?.jobDescription.orEmpty()
+        tvClinicName.text = receivedDoctor?.clinicName.orEmpty()
+        tvAddress.text = receivedDoctor?.address.orEmpty()
+        tvEmail.text = receivedDoctor?.email.orEmpty()
+        tvTelephone.text = receivedDoctor?.telephone.orEmpty()
     }
 
     private fun checkReviews() {
