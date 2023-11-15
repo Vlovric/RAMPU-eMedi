c86027933dc8384aa3e52860d74443c245e7e26b
Domagoj Hegedušić <dhegedusi21@foi.hr>
2023-11-15 18:13:36 +0100

    Revert "Made new AllReviewsActivity. Optimized code"
    This reverts commit d4ef477b3253362db892ec43b79aac0f06e4ab92.

--- Software/app/src/main/java/hr/foi/rampu/emedi/AddReviewActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/AddReviewActivity.kt
@@ -1,8 +1,7 @@
 package hr.foi.rampu.emedi
 
-import android.content.Intent
+import ReviewAdapter
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
@@ -14,8 +13,10 @@ class AddReviewActivity : AppCompatActivity() {
     private lateinit var ratingBar: RatingBar
     private lateinit var editTextReview: EditText
     private lateinit var submitButton: Button
+    private lateinit var listViewReviews: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_review)
 
@@ -23,19 +24,34 @@ class AddReviewActivity : AppCompatActivity() {
         editTextReview = findViewById(R.id.editTextReview)
         submitButton = findViewById(R.id.submitButton)
 
+
         submitButton.setOnClickListener {
             addReviewAndDisplayAllReviews()
         }
     }
 
     private fun addReviewAndDisplayAllReviews() {
+
         val userGrade = ratingBar.rating.toInt()
         val userDescription = editTextReview.text.toString()
 
+
         val userReview = Review(userGrade, userDescription)
+
         Review.addReview(userReview)
 
-        val intent = Intent(this, AllReviewsActivity::class.java)
-        startActivity(intent)
+        displayAllReviews()
+    }
+
+    private fun displayAllReviews() {
+
+        setContentView(R.layout.all_reviews)
+
+        val allReviews = Review.getAllReviews()
+
+        listViewReviews = findViewById(R.id.listViewReviews)
+
+        val adapter = ReviewAdapter(this, allReviews)
+        listViewReviews.adapter = adapter
     }
 }
