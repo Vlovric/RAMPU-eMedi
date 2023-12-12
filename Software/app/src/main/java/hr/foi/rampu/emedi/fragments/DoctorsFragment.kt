c31a8e48ca88cfe8b8c069ae63c4b139b70b92fa
Magdalena Markovinović <mmarkovin21@foi.hr>
2023-12-12 09:47:44 +0100

    Napravljena logika za pretraživanje doktora po imenu
    Nedostaje pretraživanje po imenu i prezimenu. Zahtjev za dohvaćanje nije dobro implementiran jer dohvaća doktore samo kada je cijelo ime upisano.

--- Software/app/src/main/java/hr/foi/rampu/emedi/fragments/DoctorsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/fragments/DoctorsFragment.kt
@@ -2,12 +2,16 @@ package hr.foi.rampu.emedi.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.emedi.DoctorInformationActivity
@@ -18,23 +22,52 @@ import hr.foi.rampu.emedi.database.AppDatabase
 class DoctorsFragment : Fragment() {
     private val mockDoctors = MockDataDoctor.getDemoData()
     private lateinit var recyclerView: RecyclerView
-    private lateinit var button : Button
+    private lateinit var searchTextBox : EditText
+    private lateinit var errorMessage : TextView
+    val doctorsDAO = AppDatabase.getInstance().getDoctorsDao()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_doctors, container, false)
-        return rootView
+        return inflater.inflate(R.layout.fragment_doctors, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         recyclerView = view.findViewById(R.id.rv_doctors)
+        searchTextBox = view.findViewById(R.id.tv_search_doctor)
         recyclerView.adapter = DoctorsAdapter(MockDataDoctor.getDemoData()){
             val intent = Intent(requireContext(), DoctorInformationActivity::class.java)
-            val whichDoctor = AppDatabase.getInstance().getDoctorsDao().getDoctor(it.id)
+            val whichDoctor = doctorsDAO.getDoctor(it.id)
             intent.putExtra("doctor", whichDoctor)
             startActivity(intent)
         }
         recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        searchTextBox.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val searchText = s.toString().trim()
+                getDoctorsByName(searchText)
+            }
+        })
+    }
+    private fun getDoctorsByName(name: String) {
+        val doctorsList = if (name.isNotBlank()) {
+             doctorsDAO.getDoctorByName(name)
+        } else {
+            doctorsDAO.getAllDoctors()
+        }
+        /*if(doctorsList.isEmpty()){
+            errorMessage.text = "Nije pronađen niti jedan doktor s tim imenom!"
+        }else{*/
+            recyclerView.adapter = DoctorsAdapter(doctorsList) { doctor ->
+                val intent = Intent(requireContext(), DoctorInformationActivity::class.java)
+                intent.putExtra("doctor", doctor)
+                startActivity(intent)
+            }
+//        }
     }
 }
