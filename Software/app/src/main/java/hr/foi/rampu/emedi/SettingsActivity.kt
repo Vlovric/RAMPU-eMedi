b1a4c7414ff2ac959e072ab18f19a8e2389a50d0
Domagoj Hegedušić <dhegedusi21@foi.hr>
2023-12-14 16:39:22 +0100

    Stvoreni potrebni activity-ji i klase koje će se koristiti za promjenu boje aplikacije.

--- Software/app/src/main/java/hr/foi/rampu/emedi/SettingsActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/SettingsActivity.kt
@@ -1,4 +1,3 @@
-// SettingsActivity.kt
 package hr.foi.rampu.emedi
 
 import android.content.Context
@@ -11,6 +10,8 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import hr.foi.rampu.emedi.entities.ColorPalette
 import hr.foi.rampu.emedi.helpers.TextSizeUtility
 
 class SettingsActivity : AppCompatActivity() {
@@ -18,6 +19,7 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var textSizeUtility: TextSizeUtility
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var fontSpinner: Spinner
+    private lateinit var btnAppColor: Button  // Declare btnAppColor as a property
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +46,6 @@ class SettingsActivity : AppCompatActivity() {
                 textSizeUtility.registerButton(findViewById(R.id.btnAppColor))
                 textSizeUtility.registerButton(findViewById(R.id.btnBack))
 
-
                 // Save the selected font size in SharedPreferences
                 with(sharedPreferences.edit()) {
                     putFloat("fontSize", newSize)
@@ -52,14 +53,16 @@ class SettingsActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
 
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-
-            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
         })
+
+        btnAppColor = findViewById(R.id.btnAppColor) // Initialize btnAppColor
+
+        btnAppColor.setOnClickListener {
+            startColorSelectionActivity()
+        }
 
         fontSpinner = findViewById(R.id.fontSpinner)
 
@@ -70,14 +73,13 @@ class SettingsActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                textSizeUtility.registerTextViewStyle(this@SettingsActivity,findViewById(R.id.tv_FontSize), position)
-                textSizeUtility.registerButtonStyle(this@SettingsActivity,findViewById(R.id.btnBack), position)
-                textSizeUtility.registerButtonStyle(this@SettingsActivity,findViewById(R.id.btnAppColor), position)
+                textSizeUtility.registerTextViewStyle(this@SettingsActivity, findViewById(R.id.tv_FontSize), position)
+                textSizeUtility.registerButtonStyle(this@SettingsActivity, findViewById(R.id.btnBack), position)
+                textSizeUtility.registerButtonStyle(this@SettingsActivity, btnAppColor, position) // Use btnAppColor
             }
 
             override fun onNothingSelected(parentView: AdapterView<*>?) {}
         }
-
 
         btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -86,4 +88,41 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
+    private fun startColorSelectionActivity() {
+        val intent = Intent(this, AppColorActivity::class.java)
+        startActivityForResult(intent, COLOR_SELECTION_REQUEST_CODE)
+    }
+
+    private fun applyColorsToUI(colorPalette: ColorPalette) {
+        // Apply the selected colors to the UI elements in your activity
+        // For example, change the background color, text color, etc.
+
+        // Apply background color to the whole application
+        findViewById<View>(android.R.id.content).setBackgroundColor(
+            ContextCompat.getColor(this, android.R.color.background_dark)
+        )
+
+        // Apply button background color
+        btnAppColor.setBackgroundColor(android.graphics.Color.parseColor(colorPalette.buttonColor))
+
+        // Apply text color to other elements
+        // ...
+
+        // You may need to adjust this based on your actual UI structure
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == COLOR_SELECTION_REQUEST_CODE && resultCode == RESULT_OK) {
+            val selectedPalette =
+                data?.getParcelableExtra<ColorPalette>(AppColorActivity.SELECTED_COLORS)
+            selectedPalette?.let {
+                applyColorsToUI(it)
+            }
+        }
+    }
+
+    companion object {
+        private const val COLOR_SELECTION_REQUEST_CODE = 100
+    }
 }
