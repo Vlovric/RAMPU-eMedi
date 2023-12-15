8701cc3b876c5c35354690972067c3a11357c747
David Matijanić <dmatijani21@foi.hr>
2023-12-15 12:15:57 +0100

    Nova verzija baze.

--- Software/app/src/main/java/hr/foi/rampu/emedi/database/AppDatabase.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/database/AppDatabase.kt
@@ -11,7 +11,7 @@ import hr.foi.rampu.emedi.entities.Review
 import hr.foi.rampu.emedi.entities.User
 
 @Database(
-    version=8,
+    version=9,
     entities=[User::class, Doctor::class, Review::class, BookingReason::class, Appointment::class],
     exportSchema=false
 )
