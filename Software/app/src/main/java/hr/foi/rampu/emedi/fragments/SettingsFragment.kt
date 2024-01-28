26760ea92f676bc13c3eec42146813d4483e43c9
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-01-28 14:34:11 +0100

    Promjenjena logika koda. Popravljene boje aplikacije.

--- Software/app/src/main/java/hr/foi/rampu/emedi/fragments/SettingsFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/emedi/fragments/SettingsFragment.kt
@@ -10,13 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.SeekBar
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
-import hr.foi.rampu.emedi.AppColorActivity
 import hr.foi.rampu.emedi.MainActivity
 import hr.foi.rampu.emedi.R
-import hr.foi.rampu.emedi.entities.ColorPalette
 import hr.foi.rampu.emedi.helpers.TextSizeUtility
 
 class SettingsFragment : Fragment() {
@@ -24,9 +24,6 @@ class SettingsFragment : Fragment() {
     private lateinit var textSizeUtility: TextSizeUtility
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var fontSpinner: Spinner
-    private lateinit var btnAppColor: Button
-    private var selectedColorPalette: ColorPalette? = null
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,7 +40,7 @@ class SettingsFragment : Fragment() {
 
         val seekBarFontSize: SeekBar = view.findViewById(R.id.fontSizeSeekBar)
         val btnBack: Button = view.findViewById(R.id.btnBack)
-
+        val darkTheme: CheckBox = view.findViewById(R.id.cbdarkTheme)
         val savedFontSize = sharedPreferences.getFloat("fontSize", 12f)
         seekBarFontSize.progress = (savedFontSize - 12f).toInt()
 
@@ -55,11 +52,6 @@ class SettingsFragment : Fragment() {
 
                 with(sharedPreferences.edit()) {
                     putFloat("fontSize", newSize)
-                    selectedColorPalette?.let {
-                        putInt("color1", it.color1)
-                        putInt("color2", it.color2)
-                        putInt("color3", it.color3)
-                    }
                     apply()
                 }
             }
@@ -69,11 +61,7 @@ class SettingsFragment : Fragment() {
             override fun onStopTrackingTouch(seekBar: SeekBar) {}
         })
 
-        btnAppColor = view.findViewById(R.id.btnAppColor)
 
-        btnAppColor.setOnClickListener {
-            startColorSelectionActivity()
-        }
 
         fontSpinner = view.findViewById(R.id.fontSpinner)
 
@@ -90,7 +78,6 @@ class SettingsFragment : Fragment() {
                 }
                 textSizeUtility.registerTextViewStyle(requireActivity(), view.findViewById(R.id.tv_FontSize), position)
                 textSizeUtility.registerButtonStyle(requireActivity(), view.findViewById(R.id.btnBack), position)
-                textSizeUtility.registerButtonStyle(requireActivity(), btnAppColor, position) // Use btnAppColor
             }
 
             override fun onNothingSelected(parentView: AdapterView<*>?) {}
@@ -101,6 +88,15 @@ class SettingsFragment : Fragment() {
             startActivity(intent)
             requireActivity().finish()
         }
+
+        darkTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
         changeTextSize(view)
         return view
     }
@@ -117,58 +113,10 @@ class SettingsFragment : Fragment() {
 
         textSizeUtility.registerAllButtons(
             view.findViewById(R.id.btnBack),
-            view.findViewById(R.id.btnAppColor)
             )
         textSizeUtility.registerButtonStyle(requireContext(), view.findViewById(R.id.btnBack), position)
-        textSizeUtility.registerButtonStyle(requireContext(), view.findViewById(R.id.btnAppColor), position)
-
-    }
-    private fun startColorSelectionActivity() {
-        val intent = Intent(requireContext(), AppColorActivity::class.java)
-        startActivityForResult(intent, COLOR_SELECTION_REQUEST_CODE)
-    }
-
-    private fun applyColorsToUI(colorPalette: ColorPalette) {
-
-        val color1 = colorPalette.color1
-        val color2 = colorPalette.color2
-        val color3 = colorPalette.color3
-        val button1 = view?.findViewById<Button>(R.id.btnAppColor)
-        view?.findViewById<View>(android.R.id.content)?.setBackgroundColor(color1)
-        button1?.setBackgroundColor(color2)
-        button1?.setTextColor(color3)
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == COLOR_SELECTION_REQUEST_CODE && resultCode == RESULT_OK) {
-            selectedColorPalette =
-                data?.getParcelableExtra<ColorPalette>(AppColorActivity.SELECTED_COLORS)
-            selectedColorPalette?.let {
-                applyColorsToUI(it)
-            }
-        }
-    }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        // Save the selected color palette to the bundle
-        outState.putParcelable("selectedColorPalette", selectedColorPalette)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        // Restore the selected color palette from the bundle
-        selectedColorPalette = savedInstanceState?.getParcelable("selectedColorPalette")
-        selectedColorPalette?.let {
-            applyColorsToUI(it)
-        }
-    }
-
-    companion object {
-        private const val COLOR_SELECTION_REQUEST_CODE = 100
-    }
 }
